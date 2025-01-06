eeb55d669cb587faa39cc37428c6c5e7679803fb
John H <hxjwork1@gmail.com>
2025-01-06 11:51:01 +0400

    Working on hash calculation

--- src/lib/business/snapshot_process.rs
+++ src/lib/business/snapshot_process.rs
@@ -1,5 +1,7 @@
+use std::io::Read;
 use std::path::{Path, PathBuf};
 
+use sha2::{Digest, Sha256};
 use tokio::fs;
 
 #[derive(Default)]
@@ -19,7 +21,7 @@ impl SnapshotProcess {
     async fn run_fresh(&self, source: &Path, today: &Path) -> Result<(), String> {
         let mut entries = fs::read_dir(source)
             .await
-            .map_err(|e| format!("Failed to read source directory: {}", e))?;
+            .map_err(|e| format!("Failed to read source directory: {0}", e))?;
 
         while let Some(entry) = entries
             .next_entry()
@@ -35,17 +37,17 @@ impl SnapshotProcess {
 
             if path.is_dir() {
                 std::fs::create_dir(&path_file)
-                    .map_err(|e| format!("Failed to create folder: {}", e))?;
+                    .map_err(|e| format!("Failed to create folder: {0}", e))?;
 
                 Box::pin(self.run_fresh(&path, &destination)).await?;
             } else if path.is_symlink() {
                 let link = fs::read_link(path)
                     .await
-                    .map_err(|e| format!("Failed to read symlink target: {}", e))?;
+                    .map_err(|e| format!("Failed to read symlink target: {0}", e))?;
 
                 fs::symlink(&link, path_file)
                     .await
-                    .map_err(|e| format!("Failed to create symlink in destination: {}", e))?;
+                    .map_err(|e| format!("Failed to create symlink in destination: {0}", e))?;
             } else {
                 fs::copy(&path, &destination)
                     .await
@@ -59,7 +61,7 @@ impl SnapshotProcess {
     async fn run_on_existing(&self, source: &Path, yesterday: &Path, today: &Path) -> Result<(), String> {
         let mut entries = fs::read_dir(source)
             .await
-            .map_err(|e| format!("Failed to read source directory: {}", e))?;
+            .map_err(|e| format!("Failed to read source directory: {0}", e))?;
 
         while let Some(entry) = entries
             .next_entry()
@@ -70,20 +72,50 @@ impl SnapshotProcess {
             let yesterday_destination = yesterday.join(entry.file_name());
 
             if yesterday_destination.exists() {
-                let path_file_name = path.file_name()
-                    .ok_or_else(|| format!("Failed to get folder name from path: {:?}", path))?;
-                let path_file = today.join(path_file_name);
+                if path.is_file() {
+                    let path_file_name = path.file_name()
+                        .ok_or_else(|| format!("Failed to get folder name from path: {:?}", path))?;
+                    let today_path_file = today.join(path_file_name);
 
-                // TODO: verify with an hash
-
-                fs::symlink(&yesterday_destination, path_file)
-                    .await
-                    .map_err(|e| format!("Failed to create symlink in destination: {}", e))?;
-            } else {
-                // Box::pin(self.run_fresh(&path, today)).await?;
+                    if self.same_hash(&yesterday_destination, &path).await? {
+                        fs::symlink(&yesterday_destination, today_path_file)
+                            .await
+                            .map_err(|e| format!("Failed to create symlink in destination: {0}", e))?;
+                    } else {
+                        // Box::pin(self.run_fresh(&path, today)).await?;
+                    }
+                } else {
+                    // Box::pin(self.run_fresh(&path, today)).await?;
+                }
             }
         }
 
         Ok(())
+    }
+
+    async fn same_hash(&self, source: &Path, destination: &Path) -> Result<bool, String> {
+        let source_hash = self.calculate_hash(source).await?;
+        let destination_hash = self.calculate_hash(destination).await?;
+
+        Ok(source_hash == destination_hash)
+    }
+
+    async fn calculate_hash(&self, path: &Path) -> Result<String, String> {
+        let mut file = std::fs::File::open(path)
+            .map_err(|e| format!("Failed to open file for hash calculation: {0}", e))?;
+
+        let mut hasher = Sha256::new();
+        let mut buffer = vec![0u8; 4096]; // Buffer de 4 Ko
+
+        loop {
+            let n = file.read(&mut buffer).map_err(|e| format!("Failed to read file: {0}", e))?;
+            if n == 0 {
+                break;
+            }
+            hasher.update(&buffer[..n]);
+        }
+
+        let hash = hasher.finalize();
+        Ok(format!("{:x}", hash))
     }
 }
